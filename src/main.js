c80cf107e8ec3161ff1e53613819b1a9311413f0
muzi96725 <li_yang_s@qq.com>
2019-11-19 17:35:29 +0800

    修改创建方式==> vuecli3

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import "babel-polyfill"
-import "es6-promise/auto"
+// import "babel-polyfill"
+// import "es6-promise/auto"
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -18,9 +18,7 @@ Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
   store,
-  components: { App },
-  template: '<App/>'
-});
+  render: h => h(App)
+}).$mount("#app");
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/my-project/'
+  //   : './'
+  publicPath: './'//相对路径 ('./')这样打出来的包可以被部署在任意路径
+}
